ff303984fa3510b9eef1e92a3d8cb0a947d63759
sharadpawar01 <sharadpawar01@gmail.com>
2023-02-22 09:49:19 +0530

    'Init'

---
+++ src/test/java/com/sp/bank/bankingservice/BankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.bank.bankingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
